8b72fdf121475df666417b52ceb6749df3c8fed8
sentiq22 <sentiq22@gmail.com>
2016-10-22 12:24:56 +0300

    initial, first package already using

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require(__DIR__ . '/vendor/autoload.php');
+$faker = Faker\Factory::create();
+?>
+
+<h1>Hi, my name is <?php echo $faker->name ?>!</h1>
+<h2>My address is <?php echo $faker->address ?></h2>
