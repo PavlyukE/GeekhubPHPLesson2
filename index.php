881113ca1c224083065812da29d86be2f799d8a5
sentiq22 <sentiq22@gmail.com>
2016-10-23 10:47:07 +0300

    link added

--- index.php
+++ index.php
@@ -5,3 +5,4 @@ $faker = Faker\Factory::create();
 
 <h1>Hi, my name is <?php echo $faker->name ?>!</h1>
 <h2>My address is <?php echo $faker->address ?></h2>
+<span>Visit my page: <a href="#">facebook.com</a></span>
